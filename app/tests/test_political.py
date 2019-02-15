5d0ca00c54bcbff849bdee570cd65d811a0d23e1
mimipeshy <perisndanu@gmail.com>
2019-02-15 08:22:56 +0300

    [Finishes #163971723] Update party name

--- app/tests/test_political.py
+++ app/tests/test_political.py
@@ -12,6 +12,7 @@ class PoliticalTests(BaseTests):
         response = self.client().post('/api/v2/party', data=self.add_party,
                                       content_type='application/json')
         self.assertEqual(response.status_code, 201)
+
     def test_get_all_parties(self):
         """Tests API can get all parties"""
         self.client().post('/api/v2/party', data=self.add_party,
@@ -19,6 +20,7 @@ class PoliticalTests(BaseTests):
         response = self.client().get('/api/v2/party',
                                      content_type='application/json')
         self.assertEqual(response.status_code, 200)
+
     def test_get_specific_by_id(self):
         """Tests API can get a specific party by using its id"""
         self.client().post('/api/v2/party', data=self.add_party,
@@ -30,12 +32,25 @@ class PoliticalTests(BaseTests):
 
     def test_get_wrong_political_party(self):
         self.client().get('/api/v2/party', data=self.add_party,
-                           content_type='application/json')
+                          content_type='application/json')
         response = self.client().get('/api/v2/party/10',
                                      content_type='application/json',
                                      )
         self.assertEqual(response.status_code, 404)
 
+    def test_political_update_successfully(self):
+        self.client().post('/api/v2/party', data=self.add_party,
+                           content_type='application/json')
+        response = self.client().patch('/api/v2/party/1/name', data=self.update_party,
+                                       content_type='application/json',
+                                       )
+        self.assertEqual(response.status_code, 200)
+
+    def test_political_update_forbidden(self):
+        response = self.client().patch('/api/v2/party/56/name', data=self.update_party,
+                                       content_type='application/json',
+                                       )
+        self.assertEqual(response.status_code, 404)
 
 
 if __name__ == '__main__':
