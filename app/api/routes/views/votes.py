3e8b4a32f80c92ed753e129dde1d106d81c739fb
mimipeshy <perisndanu@gmail.com>
2019-02-19 13:21:17 +0300

    [Starts #164067603] A user can cast a vote

--- app/api/routes/views/votes.py
+++ app/api/routes/views/votes.py
@@ -13,16 +13,22 @@ from app.api.blueprints import version2
 @version2.route("/votes", methods=['POST'])
 def vote_for_office():
     """a user can vote once for an office"""
-    try:
-        data = request.get_json()
-        office = data["office"]
-        candidate = data["candidate"]
-        voter = data["voter"]
-    except Exception:
+
+    data = request.get_json()
+    office = data["office"]
+    candidate = data["candidate"]
+    voter = data["voter"]
+    if not data:
         return Responses.bad_request('Invalid your json keys. Use these keys '
                                      'office, candidate, voter ')
-    if not isinstance(data['office'], str) or not isinstance(data['candidate'], str)or not isinstance(data["voter"], int):
+    if not isinstance(data['office'], int) or not isinstance(data['candidate'],int)or not isinstance(data["voter"], int):
         return Responses.bad_request('Incorrect input type'), 400
+    isCandidateRegistered= u.get_one_by_id(candidate)
+    isOfficeAvailable= g.find_office_by_name(office)
+    if isCandidateRegistered and isOfficeAvailable:
+        isVoted= v.user_voted_status(voter,office)
+        if isVoted:
+            return Responses.bad_request("You can only vote once")
 
     vote = v(office, candidate, voter)
     vote.save_vote()
@@ -30,4 +36,5 @@ def vote_for_office():
                                   "office": office,
                                   "candidate": candidate,
                                   "voter": voter}))
+
 
