e2c4a1595aeb13f5d8e41440f5bbbe6a3e676885
mimipeshy <perisndanu@gmail.com>
2019-03-09 00:48:23 +0300

    [Refactored code validation in office function]

--- app/api/routes/views/office.py
+++ app/api/routes/views/office.py
@@ -5,25 +5,37 @@ from app.api.blueprints import version2
 from app.api.responses import Responses
 from app.api.routes.models.candidate import Candidates as c
 from app.api.routes.models.office import GovernmentOffice as ofisi
-from app.api.validations.utils import Validations
+from app.api.validations.auth_validations import AuthValidations as v
 
 candidate_obj = c()
 office_obj = ofisi()
+
 
 @version2.route("/office", methods=['POST'])
 @jwt_required
 def create_an_office():
     """this creates a new office"""
-    data = request.get_json()
-    valid = Validations.create_office()
-    if valid:
-        return Validations.create_office()
-    name = data['name']
-    type = data['type']
-    final = ofisi(name, type)
-    response = final.add_political_office()
-    result = [{"id": response[0], "name": response[1], "type": response[2]}]
-    return jsonify({"msg": result}), 201
+    try:
+        data = request.get_json()
+        required_fields = ["name", "type"]
+        empty_fields = v.check_for_blank_spaces(data, required_fields=required_fields)
+        if empty_fields:
+            return v.check_for_blank_spaces(data, required_fields=required_fields)  # checks for empty fields
+        extra_fields = v.validate_extra_fields(data, required_fields=required_fields)
+        if extra_fields:
+            return v.validate_extra_fields(data, required_fields=required_fields)  # checks for extra
+            # fields
+        name = data['name']
+        type = data['type']
+        office_exist = office_obj.find_office_by_name(name)
+        if office_exist:
+            return Responses.bad_request({"Message": "Sorry, the office already exists"}), 404
+        final = ofisi(name, type)
+        response = final.add_political_office()
+        result = [{"id": response[0], "name": response[1], "type": response[2]}]
+        return jsonify({"msg": result}), 201
+    except:
+        return Responses.bad_request({"Message": 'Content-Type must be JSON.'}), 400
 
 
 @version2.route("/office", methods=['GET'])
@@ -51,7 +63,7 @@ def register_candidate():
                                      'office, candidate ')
     candidate = post_data['candidate']
     office = post_data['office']
-    candidate_object =c(candidate, office)
+    candidate_object = c(candidate, office)
     res = candidate_object.save_candidate()
     return res
 
