bdaad5a8ce20fd9f9b5a7317b8618857e1112fcb
mimipeshy <perisndanu@gmail.com>
2019-03-08 22:45:56 +0300

    [Refactor code validations]

--- app/api/routes/views/auth.py
+++ app/api/routes/views/auth.py
@@ -1,14 +1,11 @@
-from flask import make_response
-from flask import request, jsonify
+from flask import request, make_response, jsonify
 from flask_jwt_extended import create_access_token
-from werkzeug.security import check_password_hash
 
-from app.api.responses import Responses
-from app.api.blueprints import version2
-from app.api.routes.models.user import UserModel as u
-import app.api.validations.validation as validate
-from app.api.validations.validation import check_password
 import app.api.responses as errors
+from app.api.blueprints import version2
+from app.api.responses import Responses
+from app.api.routes.models.user import UserModel as u
+from app.api.validations.auth_validations import AuthValidations as v
 
 user_object = u()
 
@@ -21,31 +18,42 @@ def register_new_user():
         first_name = data['first_name']
         last_name = data['last_name']
         other_name = data['other_name']
-
         phone_number = data['phone_number']
         password = data['password']
         passportUrl = data['passportUrl']
         email = data['email']
-
     except:
         return Responses.bad_request('Check your json keys. '
                                      'first_name, last_name, other_name,'
                                      'phone_number, email, password, passportUrl', )
-    validate.validate_extra()
-    validate.check_for_strings(data, ['first_name', 'last_name', 'other_name', 'email',  'password'])
-    validate.check_for_blank_spaces(data,
-                                    ['first_name', 'last_name', 'other_name', 'email', 'phone',  'password'])
-    validate.check_email_is_valid(email)
-
+    required_fields = ['first_name', 'last_name', 'other_name', 'phone_number', 'email',
+                       'password', 'passportUrl',
+                       'is_admin']
+    error = v.validate_extra_fields(data,
+                                    required_fields=required_fields)
+    if error:
+        return v.validate_extra_fields(data, required_fields=required_fields)
+    empty = v.check_for_blank_spaces(data,
+                                     required_fields=required_fields)
+    if empty:
+        return v.check_for_blank_spaces(data, required_fields=required_fields)
+    valid_email = v.check_email_is_valid(email)
+    if not valid_email:
+        return Responses.bad_request("Invalid email url format"), 400
     user_exist = user_object.get_one_by_email(email)
     if user_exist:
-        return Responses.bad_request({"Message": "User already exists, please login"})
+        return Responses.bad_request({"Message": "User already exists, please login"}), 400
     is_admin = False
     user_ob = u(first_name, last_name, other_name, email, phone_number, password, passportUrl, is_admin)
     user_ob.save()
-    token = create_access_token(identity=email)
-    return make_response(jsonify({"Data": "User {} registered".format(email),
-                                  "token": token}), 201)
+    res = Responses.created_response({"Message": "User created successfully",
+                                      "First_name": first_name,
+                                      "Last_name": last_name,
+                                      "Phone Number": phone_number,
+                                      "Email Address": email,
+                                      "is_admin": is_admin
+                                      }), 201
+    return res
 
 
 @version2.route("/auth/login", methods=['POST'])
@@ -57,33 +65,32 @@ def login_user():
             password = data['password']
             email = data['email']
             if not email:
-
-                return Responses.not_found('Your email is missing!'),
-            if not validate.check_email_is_valid(email):
-
-                return Responses.not_found('Your email is invalid! Kindly recheck your email.')
+                return Responses.not_found({"Message": 'Your email is missing!'}), 404
+            if not v.check_email_is_valid(email):
+                return Responses.not_found({"Message": 'Your email is invalid! Kindly recheck your email.'}), 404
             user = user_object.get_one_by_email(email)
             if not user:
-                return Responses.not_found('User does not exist. Kindly register!')
+                return Responses.not_found({"Message": 'User does not exist. Kindly register!'}), 404
             else:
                 if email and password:
                     password_hash = user_object.get_password(email)[0]
-                    if check_password(password_hash, password):
+                    if v.check_password(password_hash, password):
                         token = create_access_token(identity=email)
                         if token:
                             return make_response(jsonify({"Success": 'You have logged in successfully!',
                                                           "token": token}), 200)
                     else:
-                        return Responses.bad_request('Wrong Password!')
+                        return Responses.bad_request({"Message": 'Wrong Password!'}), 404
                 else:
                     if not password:
-                        return Responses.bad_request('Your password is missing!')
-        return Responses.bad_request('Content-Type must be JSON.')
+                        return Responses.bad_request({"Message": 'Your password is missing!'}), 404
+        return Responses.bad_request({"Message": 'Content-Type must be JSON.'}), 400
     except errors.BadRequest as e:
         return e.message
     except errors.NotFound as e:
         return e.message
     except errors.Unauthorized as e:
         return e.message
-    except Exception as error:
-        return make_response(jsonify({"error": "Please provide for all the fields. Missing field: " + str(error)}), 400)
+    except Exception:
+        return Responses.bad_request(
+            {"Message": "Please provide for all the fields. Missing field:  email, password"}), 400
--- app/api/validations/utils.py
+++ app/api/validations/utils.py
@@ -11,77 +11,8 @@ from app.api.routes.models.office import GovernmentOffice as g
 political_obj = p()
 office_obj = g()
 
+
 class Validations:
-
-    @staticmethod
-    def verify_political_details():
-        """check that political party details are valid"""
-        data = request.get_json()
-        name = data['name'].strip()
-        hqAddress = data['hqAddress'].strip()
-        logoUrl = data['logoUrl'].strip()
-        if len(name) == 0:
-            return Responses.bad_request("Name cannot be empty"), 400
-        if len(hqAddress) == 0:
-            return Responses.bad_request("hqAddress name cannot be empty"), 400
-        if len(logoUrl) == 0:
-            return Responses.bad_request("LogoUrl cannot be empty"), 400
-
-    @staticmethod
-    def validate_characters():
-        "this validates a party details"
-
-        data = request.get_json()
-        name = data['name'].strip()
-        hqAddress = data['hqAddress'].strip()
-        if len(name) < 6:
-            return Responses.bad_request("Name should have more than 6 charcaters"), 400
-        if len(hqAddress) < 6:
-            return Responses.bad_request("hqAddress should have more than 6 charcaters"), 400
-        party_exist = political_obj.find_party_by_name(name)
-        if party_exist:
-            return Responses.bad_request({"Message": "Sorry, the party already exists"}), 400
-
-    @staticmethod
-    def validate_logo():
-        """this validates logoUrl format"""
-        data = request.get_json()
-        url = urlparse(data['logoUrl'])
-        if not url.scheme:
-            return Responses.bad_request("incorrect format, url should start with the format http or https"), 403
-        if not url.netloc:
-            return Responses.bad_request("url should have a valid body format eg www.twitter.com"), 403
-        if not url.path:
-            return Responses.bad_request("url should have a path format of /pic.jpg"), 403
-
-    @staticmethod
-    def validate_strings(data):
-        """ this ensures that all inputs are in the correct format"""
-        if not isinstance(data['name'], str) or not isinstance(data['hqAddress'], str) or not isinstance(
-                data['logoUrl'], str):
-            return Responses.bad_request('Ensure that all inputs are strings'), 400
-
-    @staticmethod
-    def validate_json_inputs():
-        """this check that json keys are not missing"""
-        data = request.get_json()
-        if not data:
-            return Responses.bad_request("Empty inputs in Json"), 400
-        if 'name' not in data:
-            return Responses.bad_request("Name input is missing"), 400
-        if 'hqAddress' not in data:
-            return Responses.bad_request("hqAddress input is missing"), 400
-        if 'logoUrl' not in data:
-            return Responses.bad_request("logoUrl input is missing"), 400
-
-    @staticmethod
-    def validate_extra_fields(data):
-        """this checks for extra keys in json format"""
-        required_fields = ['name', 'hqAddress', 'logoUrl']
-        if len(required_fields) < len(data.keys()):
-            for key in data:
-                if key not in required_fields:
-                    return Responses.bad_request("{} is not a valid key".format(key)), 400
 
     @staticmethod
     def create_office():
